2721ae773128b54849f3d97fadd001a31618c0da
Liam <khoasdyn@gmail.com>
2026-01-03 10:51:04 +0700

    Add PricingSimpleIcon component to homepage

--- src/pages/home-screen.tsx
+++ src/pages/home-screen.tsx
@@ -2,13 +2,14 @@ import { BookOpen01, Check, Copy01, Cube01, HelpCircle } from "@untitledui/icons
 import { Button } from "@/components/base/buttons/button";
 import { ButtonUtility } from "@/components/base/buttons/button-utility";
 import { UntitledLogoMinimal } from "@/components/foundations/logo/untitledui-logo-minimal";
+import { PricingSimpleIcon } from "@/components/marketing/pricing-sections/pricing-simple-icon";
 import { useClipboard } from "@/hooks/use-clipboard";
 
 export const HomeScreen = () => {
     const clipboard = useClipboard();
 
     return (
-        <div className="flex h-dvh flex-col">
+        <div className="flex min-h-dvh flex-col">
             <div className="flex min-h-0 flex-1 flex-col items-center justify-center px-4">
                 <div className="relative flex size-28 items-center justify-center">
                     <UntitledLogoMinimal className="size-10" />
@@ -70,6 +71,9 @@ export const HomeScreen = () => {
                     </Button>
                 </div>
             </div>
+
+            {/* Demo: Pricing Section Component */}
+            <PricingSimpleIcon />
         </div>
     );
 };
